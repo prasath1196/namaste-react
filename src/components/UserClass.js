6b2f1ab1a0b17828584f6d4c5a078590f13365c9
prasath1196 <prasath1196@gmail.com>
2024-07-12 12:23:00 +0530

    Exploring class based components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,14 +3,30 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     let { data } = this.props;
+    let { count } = this.state;
+    const increaseCount = () => {};
     return (
       <div className='h-[600px] w-[300px] rounded-lg border-s-gray-200 shadow-md'>
         <div className='content'>
           <div className='name'>This is from class component</div>
-
+          <div>{count}</div>
+          <div>
+            <button
+              onClick={() => {
+                this.setState({
+                  count: this.state.count + 1,
+                });
+              }}
+            >
+              Increase Count
+            </button>
+          </div>
           <div className='data'>{data}</div>
         </div>
       </div>
